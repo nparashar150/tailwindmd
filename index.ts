62d429c8780aa62e63fd0561ab78e8a9021b9bbe
Naman Parashar <nparashar150@gmail.com>
2024-09-08 07:32:06 +0530

    config: export types and enums

--- index.ts
+++ index.ts
@@ -1,7 +1,12 @@
 import { lexer } from "./lexer";
+import type { Token } from "./configs";
+import { TokenType, MAX_NESTING_DEPTH } from "./configs";
 
 export const generateASTFromMarkdown = (input: string) => {
   const tokens = lexer(input);
   return tokens;
 };
+
+export type { Token };
+export { TokenType, MAX_NESTING_DEPTH };
 
