af67edfdd3eca4200f3349ce61b5ce975582bb0b
Naman Parashar <nparashar150@gmail.com>
2024-10-06 00:35:42 +0530

    feat: add emit html generator feat: add string token type fix: new lines and whitespaces

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
-import { lineByLineLexer } from "./lexer";
 import type { Token } from "./configs";
-import { TokenType, MAX_NESTING_DEPTH } from "./configs";
+import { MAX_NESTING_DEPTH, TokenType } from "./configs";
+import { emitHtml } from "./generators";
+import { lineByLineLexer } from "./lexer";
 
 const markToAST = (input: string) => {
   const tokens = lineByLineLexer(input);
   return tokens;
 };
 
+export { emitHtml, markToAST, MAX_NESTING_DEPTH, TokenType };
 export type { Token };
-export { TokenType, markToAST, MAX_NESTING_DEPTH };
-
